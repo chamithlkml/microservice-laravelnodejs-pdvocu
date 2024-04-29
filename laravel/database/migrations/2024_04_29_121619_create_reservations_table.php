38d2d89846c45016269735e830b54a125fcb2052
Chamith Jayaweera <chamithlkml@gmail.com>
2024-04-29 13:29:04 +0100

    Adding Reservation migrations and resource, routes

---
+++ laravel/database/migrations/2024_04_29_121619_create_reservations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->string('reservation_code');
+            $table->string('customer_name');
+            $table->string('customer_email');
+            $table->dateTime('arrival_time');
+            $table->dateTime('departure_time');
+            $table->enum('payment_status', ['pending', 'paid']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
